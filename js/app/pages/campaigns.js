77f1ed6f31be628f607143218f6ab0b32fcf234c
MaxBachynskyi <bachinskiymaximzp92@gmail.com>
2026-01-28 12:59:12 +0100

    Update campaigns.js

--- js/app/pages/campaigns.js
+++ js/app/pages/campaigns.js
@@ -84,23 +84,37 @@ export const campaigns = {
         },
     },
     template: `
+    <Header ref="header" />
     <div class="inside-content">
-        <Header ref="header" />
         <div id='spinner' v-if="loader">
             <img src="./images/loader.gif" alt="Loading..." width="90">
         </div>
         <div class="wrapper">
             <div class="flex panel">
                 <div class="w20 al ptb20">
-                    <button class="btn-logout" @click="parent.logout()">
-                        Logout
-                    </button>
+                    <a class="btnS" href="#" @click.prevent="parent.formData={};$refs.new.active=1"><i class="fas fa-plus"></i> New</a>
                 </div>
                 <div class="w60 ptb20 ac"><input type="date" v-model="date" @change="get()" /> - <input type="date" v-model="date2" @change="get()" /></div>
                 <div class="w20 ptb30">
                     <h1>Campaigns</h1>
                 </div>
             </div>
+
+            <popup ref="new" :title="(parent.formData && parent.formData.id) ? 'Edit campaign' : 'New campaign'">
+                <div class="form inner-form">
+                    <form @submit.prevent="action()" v-if="parent.formData">
+                        <div class="row">
+                            <label>Name</label>
+                            <input type="text" v-model="parent.formData.title" required>
+                        </div>
+
+                        <div class="row">
+                            <button class="btn" v-if="parent.formData && parent.formData.id">Edit</button>
+                            <button class="btn" v-if="parent.formData && !parent.formData.id">Add</button>
+                        </div>
+                    </form>
+                </div>
+            </popup>
 
             <div class="table" v-if="data.items!=''">
                 <table>
@@ -122,7 +136,7 @@ export const campaigns = {
                         <tr v-for="(item,i) in data.items">
                             <td class="id">{{item.id}}</td>
                             <td class="id">
-
+                                <toogle v-model="item.published" @update:modelValue="parent.formData = item;action();" />
                             </td>
                             <td><router-link :to="'/campaign/'+item.id">{{item.title}}</router-link></td>
                             <td class="id">
@@ -164,4 +178,3 @@ export const campaigns = {
     </div>
     `
 };
-
